a41ceba2536b6c031685efba5632366bf9724482
stevenschling13 <stevenschlingman62@gmail.com>
2025-07-29 08:34:08 -0700

    Create PlantDetail.tsx

---
+++ src/screens/PlantDetail.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Screen from '../components/Screen';
+import Heading from '../components/Heading';
+import Text from '../components/Text';
+
+export default function PlantDetail {
+  return (
+    <Screen>
+      <Heading>Plant Detail</Heading>
+      <Text>Here you can view and edit details for a specific plant.</Text>
+    </Screen>
+  );
+}
